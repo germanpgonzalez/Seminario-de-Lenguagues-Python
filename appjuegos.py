400f2f38daf47e701bcd4c47efed82de6aef6737
germanpgonzalez <germancai@hotmail.com>
2021-04-20 10:55:28 -0300

    Desafio teoría

---
+++ appjuegos.py
@@ -0,0 +1,28 @@
+import csv
+
+# Abro el archivo CVS
+
+
+archivo = open("appstore_games.csv", "r", encoding='utf-8')
+
+csvreader = csv.reader(archivo, delimiter=',')
+
+# Imprimo el encabezado del archivo CSV
+
+encabezado = next(csvreader)
+print()
+print(encabezado)
+print()
+print()
+# Imprimo los juegos gratuitos en idioma español
+print("Juegos gratuitos en españo: ")
+print()
+for linea in csvreader:
+    if linea[12] == "ES" and linea[7] == '0':
+        print(linea[2])
+
+print()
+
+
+
+archivo.close()
